b248454d327f06b7764831913face825d818172c
bbuchsbaum <brad.buchsbaum@gmail.com>
2022-09-22 10:31:52 -0400

    places data added

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,7 @@
 ## code to prepare `DATASET` dataset goes here
 
-places_categories <- read.table("data-raw/places365.txt", header=TRUE)
-usethis::use_data(places_categories, overwrite = TRUE)
+places_categories <- read.table("data-raw/places365.txt")
+path <- places_categories$V1
+category <- sapply(strsplit(categories, "/"), function(x) x[length(x)])
+places_cat365 <- data.frame(index=1:length(path), path=path, category=category)
+usethis::use_data(places_cat365, overwrite = TRUE)
