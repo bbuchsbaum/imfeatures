88768b1bc67e10394dd6884d319e30abf106560f
bbuchsbaum <brad.buchsbaum@gmail.com>
2022-09-27 10:37:30 -0400

    resmem

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,7 @@ resmem <- NULL
 PIL <- NULL
 resmodel <- NULL
 
+#' @import reticulate
 .onLoad <- function(libname, pkgname) {
   resmem <<- reticulate::import("resmem", delay_load=TRUE)
   PIL <<- reticulate::import("PIL", delay_load=TRUE)
