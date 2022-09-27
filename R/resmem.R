30f53085bbb037c687c07c378fc38734498cf6b5
bbuchsbaum <brad.buchsbaum@gmail.com>
2022-09-27 10:55:00 -0400

    cache resmem

--- R/resmem.R
+++ R/resmem.R
@@ -3,13 +3,15 @@
 #PIL <<- reticulate::import("PIL")
 resmodel <- NULL
 
+cacheEnv <- new.env()
+
 memorability <- function(im) {
-  if (is.null(resmodel)) {
-    resmodel <<- resmem$ResMem(pretrained=TRUE)
+  if (is.null(cacheEnv$resmodel)) {
+    cacheEnv$resmodel <- resmem$ResMem(pretrained=TRUE)
   }
   img = PIL$Image$open(im) # This loads your image into memory
   img = img$convert('RGB')
-  resmodel$eval()
+  cacheEnv$resmodel$eval()
   image_x = resmem$transformer(img)
-  prediction = resmodel(image_x$view(-1L, 3L, 227L, 227L))
+  prediction = cacheEnv$resmodel(image_x$view(-1L, 3L, 227L, 227L))
 }
