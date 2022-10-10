ff0ebde439e140747bfe2b600b3c3e5c3b30ee74
bbuchsbaum <brad.buchsbaum@gmail.com>
2022-10-10 19:40:42 -0400

    multiscale entropy

---
+++ R/mse.R
@@ -0,0 +1,43 @@
+#' Multiscale entropy for images
+#'
+#'
+#' @import imager
+#' @import entropy
+#' @param im image of type `cimg` from `imager` package
+#' @param sf vector smoothing factors indicating the scales for entropy computation
+#' @param bins number of bins for computing information
+#' @export
+image_mse <- function(im, sf=c(100, 50, 8, 4, 0), bins=16) {
+  if (length(channels(im)) ==1) {
+    im <- add.colour(im)
+  }
+
+  hsvim <- RGBtoHSV(im)
+  hsvim2 <- imsplit(hsvim, "c")
+
+  ret <- lapply(sf, function(fac) {
+    i1 <- imager::isoblur(hsvim2[[1]], fac)
+    i2 <- imager::isoblur(hsvim2[[2]], fac)
+    i3 <- imager::isoblur(hsvim2[[3]], fac)
+    f1 <- try(freqs(entropy::discretize(as.vector(i1),bins)))
+    f2 <- try(freqs(entropy::discretize(as.vector(i2),bins)))
+    f3 <- try(freqs(entropy::discretize(as.vector(i3),bins)))
+
+    e1 <- if (!inherits(f1, "try-error")) {
+      entropy(f1)
+    } else NA
+
+    e2 <- if (!inherits(f2, "try-error")) {
+      entropy(f2)
+    } else NA
+
+    e3 <- if (!inherits(f3, "try-error")) {
+      entropy(f3)
+    } else NA
+
+    data.frame(fac=fac, e1=e1,e2=e2, e3=e3)
+  })
+
+  ret <- do.call(rbind, ret)
+  colMeans(as.matrix(ret[,2:4]))
+}
